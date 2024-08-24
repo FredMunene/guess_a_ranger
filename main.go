be982b2efc9b9a140992b9bbb2df1d0a28f39e34
fgitonga <mfredgitonga@gmail.com>
2024-08-24 17:44:10 +0300

    feat: update main function

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"os"
 
 	"tetris/src"
@@ -30,73 +29,14 @@ func main() {
 
 	var newTetrominoes [][]string
 
+	// resizing tetrimones
 	for _, cube := range allTetrominoes {
 		cube := src.ResizeTetri(cube)
-
 		newTetrominoes = append(newTetrominoes, cube)
-		// for _, line := range cube {
-		// 	println(line)
-		// }
-		// println("*****")
 	}
 
-	// TODO
-	// CREATE SUPER BOARD
-	InitialHeight := math.Ceil(math.Sqrt(float64(len(allTetrominoes) * 4)))
-	board := src.CreateBoard(int(InitialHeight))
-	// println(len(board))
-
-	// PLACING TETROMINOS ON BOARD
-	// RECURSION
-	// FINAL BOARD
-	Solver(board, newTetrominoes)
-}
-
-func Solver(board []string, allTetromino [][]string) {
-	// alphabets A -> Z
-	letter := 65
-	for _, tetro := range allTetromino {
-		if CanPlace(board, tetro) {
-			Place(board, letterise(tetro, letter))
-		}
-		letter++
+	board := src.SolveBoard(newTetrominoes)
+	for _, row := range board {
+		fmt.Println(row)
 	}
-
-	for _, line := range board {
-		println(line)
-	}
-}
-
-func CanPlace(board []string, tetro []string) bool {
-	count := 0
-	for i := range len(board) {
-		for j := range len(board) {
-			if board[i][j] == '.' && tetro[i][j] == '#' {
-				count++
-			}
-		}
-	}
-	return count == 4
-}
-
-func Place(board []string, tetro []string) []string {
-	for i := range len(board) {
-		board[i], tetro[i] = tetro[i], board[i]
-	}
-
-	return board
-}
-
-func letterise(tetro []string, letter int) []string {
-	tetromino := []string{}
-	for i := range len(tetro) {
-		line := ""
-		for j := range len(tetro) {
-			if tetro[i][j] == '#' {
-				line += string(letter)
-			}
-		}
-		tetromino = append(tetromino, line)
-	}
-	return tetromino
 }
